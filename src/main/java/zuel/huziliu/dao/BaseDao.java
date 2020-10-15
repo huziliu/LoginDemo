41f9b5377d64ec2741bd7bb036b1e805799464ad
huziliu <724938718@qq.com>
2020-10-15 23:23:30 +0800

    Update

---
+++ src/main/java/zuel/huziliu/dao/BaseDao.java
@@ -0,0 +1,8 @@
+package zuel.huziliu.dao;
+//操作数据库的公共类
+public class BaseDao {
+    private static String driver;
+    private static String url;
+    private static String username;
+    private static String password;
+}
---
+++ src/main/java/zuel/huziliu/pojo/User.java
@@ -0,0 +1,150 @@
+package zuel.huziliu.pojo;
+
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String userCode;
+    private String user_Name;
+    private String userPassword;
+    private int gender;
+    private Date birthday;
+    private String phone;
+    private String address;
+    private int userRole;
+    private int createdBy;
+    private Date creationDate;
+    private int modifyBy;
+    private Date modifyDate;
+
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(int id, String userCode, String user_Name, String userPassword, int gender, Date birthday, String phone, String address, int userRole, int createdBy, Date creationDate, int modifyBy, Date modifyDate) {
+        this.id = id;
+        this.userCode = userCode;
+        this.user_Name = user_Name;
+        this.userPassword = userPassword;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.address = address;
+        this.userRole = userRole;
+        this.createdBy = createdBy;
+        this.creationDate = creationDate;
+        this.modifyBy = modifyBy;
+        this.modifyDate = modifyDate;
+    }
+
+    public Integer getAge() {
+        Integer age=(new Date().getYear())-birthday.getYear();
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUser_Name() {
+        return user_Name;
+    }
+
+    public void setUser_Name(String user_Name) {
+        this.user_Name = user_Name;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(int userRole) {
+        this.userRole = userRole;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(int modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
